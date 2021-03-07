4cdaea55c8962501e4af397a1a716233391b9b7f
alcos94 <albertcr1994@gmail.com>
2021-03-07 12:00:51 +0100

    README file modified

--- README.txt
+++ README.txt
@@ -2,4 +2,4 @@ Project about Natural Language Processing
 allows to detect messages that are fake
 
 it is needed to install nltk in your env to run it
-pip install --allow-all-external --allow-unverified ntlk nltk
+pip install nltk
--- main.py
+++ main.py
@@ -1,20 +1,1 @@
-
-# Natural Language Toolkit
-import nltk
-
-# use of a Data Set from UCI --> SMS Spam Collection Data Set
-# Data set with messages label as Spam of Ham (good ones)
-
-def main():
-    print('Hello World')
-
-    #open the file of the Messages
-    f = open('SMSSpamCollection')
-    # we get the messages into a variable
-    messages = [line.rstrip() for line in f]
-    print(len(messages))
-
-
-
-if __name__ == "__main__":
-    main()
+# Natural Language Toolkitimport nltk# use of a Data Set from UCI --> SMS Spam Collection Data Set# Data set with messages label as Spam of Ham (good ones)def main():    print('Hello World')    #open the file of the Messages    f = open('SMSSpamCollection')    # we get the messages into a list variable,we split the text by the endsapce of each message    messages = [line.rstrip() for line in f] # spaces at the end of the strin are removed    print(len(messages))    print(messages[0]) # first message ( first element of the list )    print(type(messages))if __name__ == "__main__":    main()
