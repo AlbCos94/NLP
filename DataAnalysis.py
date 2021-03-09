8e8609511d7896e73284b356fb3dc856e8f457d6
alcos94 <albertcr1994@gmail.com>
2021-03-09 21:09:51 +0100

    script added for data analysis

---
+++ DataAnalysis.py
@@ -0,0 +1,12 @@
+# Script for Data Analysis
+# the data set of messages is gonna be analysed
+
+
+# print the "n" first messages of the DataSet
+def PrintNMessages(messages, n):
+    for mess_number, message in enumerate(messages[:n]):
+        print(mess_number,message)
+
+
+
+
