350be58c893ddd168fd018ff1c26f8b8111c577e
alcos94 <albertcr1994@gmail.com>
2021-03-13 10:36:10 +0100

    data frame built up using the message file

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,6 +1,11 @@
 # Script for Data Analysis
 # the data set of messages is gonna be analysed
 
+import numpy as np
+import pandas as pd
+#import seaborn as sns
+# import matplotlib.pyplot as plt
+
 
 # print the "n" first messages of the DataSet
 def PrintNMessages(messages, n):
@@ -8,5 +13,23 @@ def PrintNMessages(messages, n):
         print(mess_number,message)
 
 
+# do to the spacing ( there are more than one space between label and message), we can see it is beeing used the tab separator between label and message
+# therefore we have a TSV (tab separator value) file --> text format for storing data in a tabular structure. (each value of a record is separated from the next by a tab character).
+def CreateDataFrame(file_name):
+    return pd.read_csv(file_name, sep='\t', names=['label','message'])
+
+def GetNFirstElementsDataFrame(data_frame, num):
+    print(data_frame.head(num))
+
+def DescribeDataFrame(data_frame):
+    print(data_frame.describe())
+
+def DescribeByLabel(data_frame, label_denotation):
+    print(data_frame.groupby(label_denotation).describe())
 
 
+#def AnalyzeLengthMessages(data_frame):
+    #data_frame['length'] = data_frame['message'].apply(len) # we add a new feature to the data frame, that is the length of the message
+#    GetNFirstElementsDataFrame( data_frame, 3)
+    #plt.plot(data_frame['length'])
+    #data_frame['length'].plot.hist(bins=50)
--- README.txt
+++ README.txt
@@ -3,3 +3,16 @@ allows to detect messages that are fake
 
 it is needed to install nltk in your env to run it
 pip install nltk
+nltk --> The Natural Language Toolkit (NLTK) is a platform used for building Python programs that work with human language data for applying in statistical natural language processing (NLP). It contains text processing libraries for tokenization, parsing, classification, stemming, tagging and semantic reasoning.
+
+it is needed to install pandas in your env
+pip install pandas
+pandas -->  software library written for the Python programming language for data manipulation and analysis. In particular, it offers data structures and operations for manipulating numerical tables and time series.
+
+it is needed to install matplotlib in your env
+pip install matplotlib
+matplotlib --> Matplotlib is a plotting library for the Python programming language and its numerical mathematics extension NumPy. It provides an object-oriented API for embedding plots into applications using general-purpose GUI toolkits like Tkinter, wxPython, Qt, or GTK+.
+
+it is needed to install seaborn in your en
+pip install seaborn
+seaborn --> Seaborn is a Python data visualization library based on matplotlib. It provides a high-level interface for drawing attractive and informative statistical graphics.
